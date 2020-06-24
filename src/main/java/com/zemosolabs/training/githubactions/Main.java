0765150dd696c42f95115e0f951a70e7c5fa5907
Mohammed Atif <atif@zemosolabs.com>
2020-06-24 16:28:03 +0530

    Sample product

--- src/main/java/com/zemosolabs/training/githubactions/Main.java
+++ src/main/java/com/zemosolabs/training/githubactions/Main.java
@@ -3,7 +3,8 @@ package com.zemosolabs.training.githubactions;
 public class Main {
 
     public static void main(String[] args) {
-
+        Calculator calculator = new SimpleCalculatorImpl();
+        System.out.println("Product is "+calculator.product(1, 2, 3, 4));
     }
 
 }
